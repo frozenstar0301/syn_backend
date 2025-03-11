264dc1e4de7567afa7d3909dfee000facb77947f
frozenstar0301 <150285830+frozenstar0301@users.noreply.github.com>
2025-03-11 16:37:13 +0000

    console.log

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -15,6 +15,8 @@ const firebaseConfig = {
   measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
+console.log("firebaseConfig::: ", firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
